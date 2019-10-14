da0915afb03edbf8948e5938e87ec282c730c747
Ricardo Piro-Rael <ricardo@hint.com>
2019-10-14 00:01:17 -0600

    Customizations to timeline

--- src/components/centerline.js
+++ src/components/centerline.js
@@ -1,19 +1,9 @@
 import React from 'react'
 const Centerline = ({ offsetToCenter }) =>
-  <div className={`col-sm-1 ${offsetToCenter && 'offset-5'}`}>
-    <div
-      className="bg-primary"
-      style={{
-        minHeight: '150px',
-        borderWidth: '0 25px',
-        borderStyle: 'solid',
-        borderColor: '#FFFFFF',
-        alignItems: 'center',
-        alignContent: 'center',
-        //display: 'flex'
-      }}
-    >
-      <div style={{ borderwidth: '1px', borderColor: '#000000', borderStyle: 'solid', borderRadius: '50%', bgColor: '#FFFFFF', minHeight: '20px', minWidth: '20px', position: 'relative' }}></div>
+  <div className={`col-sm-1 ${offsetToCenter && 'offset-5'}`} data-aos="fade-up">
+    <div className={`centerline-crossbar ${offsetToCenter || 'offset-left'}`}></div>
+    <div className="centerline-circle"></div>
+    <div className="centerline-line bg-primary">
     </div>
   </div>
 
--- src/components/timeline_post.js
+++ src/components/timeline_post.js
@@ -2,12 +2,12 @@ import React from 'react'
 import Carousel from './carousel'
 
 const TimelinePost = ({ title, description, carousel }) =>
-  <div className="col-sm-5">
-    <div className="card bg-dark" data-aos="fade-up" style={{ maxHeight: '150px', overflow: 'hidden', boxShadow: '5px 5px 9px 9px #888' }}>
+  <div className="col-sm-5" data-aos="fade-up" style={{ zIndex: 1 }}>
+    <div className="card bg-dark timeline-post">
       <Carousel className="card-img" carousel={carousel} />
       <div className="card-img-overlay">
-        <h5 className="card-title">{title}</h5>
-        <p className="card-text">{description}</p>
+        <h5 className="card-title outlined-text">{title}</h5>
+        <p className="card-text outlined-text">{description}</p>
       </div>
     </div>
   </div>
