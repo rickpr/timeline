fd173c14c8298ff5dc888c1479d2f0e29f92239e
Adalida Baca <adalida.baca@gmail.com>
2020-04-05 23:24:22 -0600

    Adjusted colors

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -6,15 +6,14 @@ const Header = () => {
   const collapsibleId = 'navbarSupportedContent'
   return (
     <header>
-      <nav className="navbar navbar-expand-lg navbar-light">
+      <nav className="navbar navbar-expand-lg navbar-light navbar-custom">
         <MobileHamburger collapsibleId={collapsibleId} />
         <div className="collapse navbar-collapse" id={collapsibleId} >
-          <ul className="navbar-nav mr-auto">
-            <NavLink href="/" text="Home" />
-            <NavLink href="/adalida" text="Adalida's Page" />
-            <NavLink href="/ricardo" text="Ricardo's Page" />
-            <NavLink href="/us" text="Us Page" />
-          </ul>
+          <div className="navbar-nav row">
+            <NavLink href="/adalida" text="Adalida" width={4} />
+            <NavLink href="/" text="Us" width={4} />
+            <NavLink href="/ricardo" text="Ricardo" width={4} />
+          </div>
         </div>
       </nav>
     </header>
