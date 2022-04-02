02c4fac1f497896acf434ec4ec67f904c725ec28
Ricardo Piro-Rael <fdisk87@yahoo.com>
2022-04-02 00:35:15 -0700

    Add inverting color to menu text

--- src/components/adalida_page/footer.js
+++ src/components/adalida_page/footer.js
@@ -15,35 +15,38 @@ const Footer = () => {
   }
   const linkStyle = { color: '#FFFFFF', pointerEvents: 'all' }
   return (
-    <nav className='adalida-footer' style={borderStyles}>
-      <div style={{ display: 'flex', justifyContent: 'space-between', width: '80%', margin: '0 auto' }}>
-        <a href='#' style={linkStyle}><IconFileInfo /></a>
-        <a
-          href='https://dribbble.com/adalida-baca'
-          style={linkStyle}
-          target='_blank'
-          rel='noreferrer'
-        >
-          <IconBrandDribbble />
-        </a>
-        <a
-          href='https://www.linkedin.com/in/adalidabaca'
-          style={linkStyle}
-          target='_blank'
-          rel='noreferrer'
-        >
-          <IconBrandLinkedin />
-        </a>
-        <a
-          href='mailto:adalida.baca@gmail.com'
-          style={linkStyle}
-          target='_blank'
-          rel='noreferrer'
-        >
-          <IconMail />
-        </a>
-      </div>
-    </nav>
+    <>
+      <nav className='adalida-footer' style={{ mixBlendMode: 'difference' }}>
+        <div style={{ display: 'flex', justifyContent: 'space-between', width: '80%', margin: '0 auto' }}>
+          <a href='#' style={linkStyle}><IconFileInfo /></a>
+          <a
+            href='https://dribbble.com/adalida-baca'
+            style={linkStyle}
+            target='_blank'
+            rel='noreferrer'
+          >
+            <IconBrandDribbble />
+          </a>
+          <a
+            href='https://www.linkedin.com/in/adalidabaca'
+            style={linkStyle}
+            target='_blank'
+            rel='noreferrer'
+          >
+            <IconBrandLinkedin />
+          </a>
+          <a
+            href='mailto:adalida.baca@gmail.com'
+            style={linkStyle}
+            target='_blank'
+            rel='noreferrer'
+          >
+            <IconMail />
+          </a>
+        </div>
+      </nav>
+      <nav className='adalida-footer' style={borderStyles} />
+    </>
   )
 }
 
--- src/components/adalida_page/header.js
+++ src/components/adalida_page/header.js
@@ -9,36 +9,40 @@ const Header = () => {
   const borderColor = useContext(ThemeContext).primary || '#FFFFFF'
   const borderStyle = `0.25vh solid ${borderColor}`
   const linkStyle = { color: '#FFFFFF', pointerEvents: 'all' }
-  const navbarStyles = {
+  const borderStyles = {
     borderLeft: borderStyle,
     borderRight: borderStyle,
     borderTop: borderStyle
   }
 
   return (
-    <nav className='adalida-header' style={navbarStyles}>
-      <div
-        style={{
-          display: 'flex',
-          justifyContent: 'space-between',
-          alignItems: 'center',
-          width: '95%',
-          margin: '0 auto',
-          fontSize: '3vh'
-        }}
-      >
-        <div>
-          <Link to='/adalida/apps' style={linkStyle}>
-            ADALIDA
-          </Link>
+    <>
+      <nav className='adalida-header' style={{ mixBlendMode: 'difference' }}>
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            width: '95%',
+            margin: '0 auto',
+            fontSize: '3vh',
+            mixBlendMode: 'difference'
+          }}
+        >
+          <div>
+            <Link to='/adalida/apps' style={linkStyle}>
+              ADALIDA
+            </Link>
+          </div>
+          <div>
+            <Link to='/adalida' style={linkStyle}>
+              ABOUT
+            </Link>
+          </div>
         </div>
-        <div>
-          <Link to='/adalida' style={linkStyle}>
-            ABOUT
-          </Link>
-        </div>
-      </div>
-    </nav>
+      </nav>
+      <nav className='adalida-header' style={borderStyles} />
+    </>
   )
 }
 
