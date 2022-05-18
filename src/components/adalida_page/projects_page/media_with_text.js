74bc0059af525cead646b4eb14c4db5e9b728d51
Ricardo Piro-Rael <fdisk87@yahoo.com>
2022-05-17 22:48:41 -0400

    Add Inconsolata font, improve layout

--- src/components/adalida_page/projects_page/media_with_text.js
+++ src/components/adalida_page/projects_page/media_with_text.js
@@ -4,35 +4,36 @@ import PropTypes from 'prop-types'
 import Image from 'components/image'
 
 const MediaWithText = ({ media, text, reversed = false }) => {
-  const style = { minWidth: '50ch', maxHeight: '82vh' }
-  const objectPosition = reversed ? '0' : '100%'
+  const maxHeight = '82vh'
+  const style = { minWidth: '50ch', maxHeight }
   const makeMediaTag = () => {
     if (media.endsWith('.png')) {
-      return <Image path={media} style={style} imgStyle={{ objectPosition }} />
+      return <Image path={media} style={style} />
     }
     if (media.endsWith('.mp4')) {
-      return <video src={media} autoPlay loop muted playsInline style={{ height: '82vh' }} />
+      return <video src={media} autoPlay loop muted playsInline style={{ maxHeight }} />
     }
     if (media.endsWith('.gif')) {
-      return <img alt='' src={media} type='video/mp4' autoPlay style={{ height: '82vh' }} />
+      return <img alt='' src={media} type='video/mp4' autoPlay style={{ maxHeight}} />
     }
 
     throw new Error(`Could not identify type of media ${media}`)
   }
   const mediaTag = makeMediaTag()
-  const mediaPlaceContent = reversed ? 'flex-start' : 'flex-end'
-  const flex = '2 0 49%'
+  const flex = '2 2 49%'
   const imageTag = (
-    <div key='image' style={{ display: 'flex', flex, flexShrink: 0, gridArea: 'image', placeContent: mediaPlaceContent, placeItems: 'center' }}>
+    <div
+      key='image'
+      style={{ display: 'flex', flex: '2 2 49%', placeContent: 'center', placeItems: 'center', maxWidth: 'max-content' }}
+    >
       {mediaTag}
     </div>
   )
-  const textPlaceContent = reversed ? 'flex-end' : 'flex-start'
-  const textTag = <div key='text' style={{ display: 'flex', flex, placeContent: textPlaceContent }}>{text}</div>
+  const textTag = <div key='text' style={{ display: 'flex', flex, placeContent: 'center' }}>{text}</div>
 
   const flexWrap = reversed ? 'wrap' : 'wrap-reverse'
   return (
-    <div style={{ display: 'flex', flexWrap, gap: '2%' }}>
+    <div style={{ display: 'flex', flexWrap, gap: '2%', placeContent: 'center', margin: '0 auto' }}>
       {reversed ? [textTag, imageTag] : [imageTag, textTag]}
     </div>
   )
