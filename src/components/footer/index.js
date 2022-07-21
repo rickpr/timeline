3c25a99f66872c15fda0de390f630b63941a0f2a
Ricardo Piro-Rael <fdisk87@yahoo.com>
2022-07-21 04:10:15 -0700

    Fix Helios footer spacing

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -2,38 +2,38 @@ import React from 'react'
 import { StaticImage } from 'gatsby-plugin-image'
 import { IconBrandGithub, IconBrandLinkedin, IconTerminal2 } from '@tabler/icons'
 
+import 'sass/footer.scss'
+
 const Footer = () =>
   <footer>
-    <nav>
-      <div className='footer-nav'>
-        <div>
-          Adalida Baca<br />
-          <a href='https://github.com/adalidabaca' target='_blank' rel='noreferrer'>
-            <IconBrandGithub />
-          </a>
-          <a href='https://linkedin.com/in/adalidabaca' target='_blank' rel='noreferrer'>
-            <IconBrandLinkedin />
-          </a>
-          <a href='https://www.hackerrank.com/profile/adalida_baca' target='_blank' rel='noreferrer'>
-            <IconTerminal2 />
-          </a>
-        </div>
-        <div>
-          <br />
-          <span><StaticImage src='../../images/sun-moon.png' /></span>
-        </div>
-        <div>
-          Ricardo Piro-Rael<br />
-          <a href='https://github.com/rickpr' target='_blank' rel='noreferrer'>
-            <IconBrandGithub />
-          </a>
-          <a href='https://linkedin.com/in/rickpr' target='_blank' rel='noreferrer'>
-            <IconBrandLinkedin />
-          </a>
-          <a href='https://www.hackerrank.com/profile/fdisk87' target='_blank' rel='noreferrer'>
-            <IconTerminal2 />
-          </a>
-        </div>
+    <nav className='footer-nav'>
+      <div>
+        Adalida Baca<br />
+        <a href='https://github.com/adalidabaca' target='_blank' rel='noreferrer'>
+          <IconBrandGithub />
+        </a>
+        <a href='https://linkedin.com/in/adalidabaca' target='_blank' rel='noreferrer'>
+          <IconBrandLinkedin />
+        </a>
+        <a href='https://www.hackerrank.com/profile/adalida_baca' target='_blank' rel='noreferrer'>
+          <IconTerminal2 />
+        </a>
+      </div>
+      <div>
+        <br />
+        <span><StaticImage src='../../images/sun-moon.png' /></span>
+      </div>
+      <div>
+        Ricardo Piro-Rael<br />
+        <a href='https://github.com/rickpr' target='_blank' rel='noreferrer'>
+          <IconBrandGithub />
+        </a>
+        <a href='https://linkedin.com/in/rickpr' target='_blank' rel='noreferrer'>
+          <IconBrandLinkedin />
+        </a>
+        <a href='https://www.hackerrank.com/profile/fdisk87' target='_blank' rel='noreferrer'>
+          <IconTerminal2 />
+        </a>
       </div>
     </nav>
   </footer>
