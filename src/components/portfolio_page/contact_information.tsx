2656fac36534d66a9df0c5af461f66592b6b6c7e
Ricardo Piro-Rael <fdisk@fdisk.co>
2024-06-30 22:31:04 -0700

    Switch order of elements on portfolio page

--- src/components/portfolio_page/contact_information.tsx
+++ src/components/portfolio_page/contact_information.tsx
@@ -18,14 +18,14 @@ const ContactInformation = (): React.ReactElement | null => {
   return (
     <div className='contact-information'>
       <div className='title'>👋 Hi, I&apos;m Adalida Baca</div>
-      <div className='glass icon-container'>
-        <span style={{ marginLeft: '12px' }}>{typewriter}</span>
-        <BrandIcon href='https://linkedin.com/in/adalidabaca' Icon={IconBrandLinkedin} size={iconSize} />
-      </div>
       <h2 className='subtitle'>
         I design functional, human-centered products that solve user needs and drive business success by leveraging
         philosophical insights and technical communication expertise.
       </h2>
+      <div className='glass icon-container'>
+        <span style={{ marginLeft: '12px' }}>{typewriter}</span>
+        <BrandIcon href='https://linkedin.com/in/adalidabaca' Icon={IconBrandLinkedin} size={iconSize} />
+      </div>
     </div>
   )
 }
